7e77c3951d3b6677c71b228d38d3f093d2672616
Aloysiuss67 <bencurran1@hotmail.com>
2017-05-05 10:58:33 +1200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+//assume motor 1 is on the left so that the +ve direction is forward
+//assume motor 2 is on the right so that the -ve direction is forward
+
+int main (){
+  init();
+  
+  while(true){
+    take_picture
